852b8ea2b4072fef8dac2af2b388e51ae4d43c25
Fran.vargas(ruiz) <4354486+fvarrui@users.noreply.github.com>
2019-12-05 08:42:00 +0000

    A localdate xmladapter

--- src/main/java/dad/hola/jaxb/Main.java
+++ src/main/java/dad/hola/jaxb/Main.java
@@ -1,6 +1,7 @@
 package dad.hola.jaxb;
 
 import java.io.File;
+import java.time.LocalDate;
 
 import dad.hola.jaxb.model.Agenda;
 import dad.hola.jaxb.model.Persona;
@@ -16,12 +17,14 @@ public class Main {
 		Persona p1 = new Persona();
 		p1.setNombre("Chuck");
 		p1.setApellidos("Norris");
+		p1.setFechaNacimiento(LocalDate.of(1945, 12, 3));
 		p1.getTelefonos().add(new Telefono(922000001L));
 		p1.getTelefonos().add(new Telefono(922000002L));
 
 		Persona p2 = new Persona();
 		p2.setNombre("Sylvester");
 		p2.setApellidos("Stallone");
+		p2.setFechaNacimiento(LocalDate.of(1951, 7, 14));
 		p2.getTelefonos().add(new Telefono(666000001L));
 		p2.getTelefonos().add(new Telefono(666000002L));
 		p2.getTelefonos().add(new Telefono(666000003L));
--- src/main/java/dad/hola/jaxb/model/Persona.java
+++ src/main/java/dad/hola/jaxb/model/Persona.java
@@ -1,17 +1,22 @@
 package dad.hola.jaxb.model;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
 import javax.xml.bind.annotation.XmlAttribute;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlType;
+import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
+
+import dad.hola.jaxb.utils.LocalDateAdapter;
 
 @XmlType
 public class Persona {
 
 	private String nombre;
 	private String apellidos;
+	private LocalDate fechaNacimiento;
 	private List<Telefono> telefonos = new ArrayList<Telefono>();
 
 	@XmlAttribute
@@ -32,6 +37,16 @@ public class Persona {
 		this.apellidos = apellidos;
 	}
 
+	@XmlAttribute
+	@XmlJavaTypeAdapter(value = LocalDateAdapter.class)
+	public LocalDate getFechaNacimiento() {
+		return fechaNacimiento;
+	}
+
+	public void setFechaNacimiento(LocalDate fechaNacimiento) {
+		this.fechaNacimiento = fechaNacimiento;
+	}
+
 	@XmlElement
 	public List<Telefono> getTelefonos() {
 		return telefonos;
@@ -40,7 +55,7 @@ public class Persona {
 	public void setTelefonos(List<Telefono> telefono) {
 		this.telefonos = telefono;
 	}
-	
+
 	@Override
 	public String toString() {
 		return getNombre() + " " + getApellidos() + " (" + getTelefonos().size() + " teléfonos registrados)";
